1bf8daee648b079d392b47f00a8e1c5705280fe5
AkiraTakahashi0906 <oasis0906@gmail.com>
2020-01-08 20:06:27 +0900

    Initial Commit

---
+++ swift-UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-UI
+//
+//  Created by akira_takahashi on 2020/01/08.
+//  Copyright © 2020 akira_takahashi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
